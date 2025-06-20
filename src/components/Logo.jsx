d608a5c4f028c8c104bdaab91a9bc60bacf2ecb5
noboKumar <nobokumar227@gmail.com>
2025-06-20 14:28:03 +0600

    set up folder and route

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/logo.png';
+
+const Logo = () => {
+    return (
+        <div>
+            <img src={logo} alt="logo" />
+            <h1>Zap Shift</h1>
+        </div>
+    );
+};
+
+export default Logo;
