d205a0020eb09c8b8e54ea88efb54c3126f62a67
noboKumar <nobokumar227@gmail.com>
2025-06-22 11:34:58 +0600

    add sales team marquee

---
+++ src/components/Home/SalesTeam/SalesTeam.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import HeadTitle from "../../Shared/HeadTitle";
+import Marquee from "react-fast-marquee";
+import salesIcon1 from "../../../assets/brands/amazon.png";
+import salesIcon3 from "../../../assets/brands/casio.png";
+import salesIcon4 from "../../../assets/brands/moonstar.png";
+import salesIcon5 from "../../../assets/brands/randstad.png";
+import salesIcon6 from "../../../assets/brands/start-people 1.png";
+import salesIcon7 from "../../../assets/brands/start.png";
+
+const SalesTeam = () => {
+  const partners = [
+    salesIcon1,
+    salesIcon3,
+    salesIcon4,
+    salesIcon5,
+    salesIcon6,
+    salesIcon7,
+  ];
+  return (
+    <div className="space-y-8 w-10/12 mx-auto">
+      <div className="text-center">
+        <HeadTitle title={"We've helped thousands of sales teams"}></HeadTitle>
+      </div>
+      <div>
+        <Marquee speed={40}>
+          {partners.map((logo, index) => (
+            <img
+              className="mr-10 h-10 w-auto object-cover"
+              key={index}
+              src={logo}
+              alt="partners-logo"
+            />
+          ))}
+        </Marquee>
+      </div>
+    </div>
+  );
+};
+
+export default SalesTeam;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Slider from "../../components/Home/Slider/Slider";
 import HowItWorks from "../../components/Home/HowItWorks/HowItWorks";
 import OurServices from "../../components/Home/OurServices/OurServices";
+import SalesTeam from "../../components/Home/SalesTeam/SalesTeam";
 const Home = () => {
   return (
-    <div className="space-y-10">
+    <div className="space-y-20">
       <Slider></Slider>
       <HowItWorks></HowItWorks>
       <OurServices></OurServices>
+      <SalesTeam></SalesTeam>
     </div>
   );
 };
